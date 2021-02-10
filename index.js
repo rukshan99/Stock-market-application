6ddee4bcfd07b8421ffa12ad87091c80a8238679
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2021-02-10 17:50:02 +0530

    Basic Bootstrap modifications

--- index.js
+++ index.js
@@ -12,11 +12,23 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 // Setting up handlebar routes
+
+// home route
 app.get('/', function (req, res) {
     res.render('home');
+});
+// about me route
+app.get('/about', function (req, res) {
+    res.render('about');
+});
+// contact me route
+app.get('/contact', function (req, res) {
+    res.render('contact');
 });
 
 // Setting up the static folder
 app.use(express.static(path.join(__dirname, 'public')));
+// Setting up the static images folder
+app.use(express.static('views/layouts/images'));
 
 app.listen(PORT, () => console.log('Server listening on port ' + PORT + '...'));
